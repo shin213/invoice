1c6407d570317ea57ae0e6452bd2f6d68dde3bd3
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-01-06 21:54:00 +0900

    add: user cards

--- front/src/components/pages/Requests.tsx
+++ front/src/components/pages/Requests.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import UserListCard from '../molecules/UserListCard'
+import SocialProfileWithImage from '../molecules/UserCard1'
+import SocialProfileWithImageHorizontal from '../molecules/UserCard2'
 import LoginTemplate from '../templates/LoginTemplate'
 
 const RequestsPage: React.VFC = () => (
   <LoginTemplate>
     <div>This is Requests.</div>
+    <UserListCard />
+    <SocialProfileWithImage />
+    <SocialProfileWithImageHorizontal />
   </LoginTemplate>
 )
 
