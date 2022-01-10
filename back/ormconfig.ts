e380b06a8c1534b63dc548d622b74bb11231dd1f
Kasega0 <shimomura@hapis.k.u-tokyo.ac.jp>
2022-01-10 17:15:20 +0900

    fix: type orm config error

--- back/ormconfig.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigModule } from '@nestjs/config'
-import typeOrmConfig from './src/type_orm_config'
-
-ConfigModule.forRoot({
-  isGlobal: true,
-  load: [typeOrmConfig],
-})
-
-export default typeOrmConfig()
