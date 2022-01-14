bbe418c00e548e3fab1997baa2b0f12de9e1e0fb
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-01-14 18:37:26 +0900

    add: graphql を導入

--- front/src/pages/requests.tsx
+++ front/src/pages/requests.tsx
@@ -1,16 +1,35 @@
 import React from 'react'
-import UserListCard from '../molecules/UserListCard'
-import SocialProfileWithImage from '../molecules/UserCard1'
-import SocialProfileWithImageHorizontal from '../molecules/UserCard2'
-import LoginTemplate from '../templates/LoginTemplate'
+import UserListCard from '../components/molecules/UserListCard'
+import SocialProfileWithImage from '../components/molecules/UserCard1'
+import SocialProfileWithImageHorizontal from '../components/molecules/UserCard2'
+import LoginTemplate from '../components/templates/LoginTemplate'
+import { useCompaniesQuery } from '../generated/graphql'
 
-const RequestsPage: React.VFC = () => (
-  <LoginTemplate>
-    <div>This is Requests.</div>
-    <UserListCard />
-    <SocialProfileWithImage />
-    <SocialProfileWithImageHorizontal />
-  </LoginTemplate>
-)
+const RequestsPage: React.VFC = () => {
+  const { loading, error, data } = useCompaniesQuery()
+  if (loading || error) {
+    if (error) {
+      console.error(error)
+    }
+    console.log(data)
+    return (
+      <LoginTemplate>
+        <div>This is Requests.</div>
+        <UserListCard />
+        <SocialProfileWithImage />
+        <SocialProfileWithImageHorizontal />
+      </LoginTemplate>
+    )
+  }
+  console.log(data)
+  return (
+    <LoginTemplate>
+      <div>This is Requests.</div>
+      <UserListCard />
+      <SocialProfileWithImage />
+      <SocialProfileWithImageHorizontal />
+    </LoginTemplate>
+  )
+}
 
 export default RequestsPage
