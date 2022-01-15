9003b078f57f36cb5fe64717b2835ab948dc932a
hamadatakaki <i16hamadat@gmail.com>
2022-01-16 04:53:39 +0900

    upd: define Invoice table and update module

--- back/src/invoices/invoice.ts
+++ back/src/invoices/invoice.ts
@@ -1,1 +1,41 @@
-export class Invoice {}
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Field, ID, ObjectType } from '@nestjs/graphql'
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  JoinColumn,
+} from 'typeorm'
+import { Company } from 'src/companies/company'
+import { User } from 'src/users/user'
+
+enum InvoiceStatus {
+  NOT_REQUESTED,
+  REQUESTED,
+  COMPLETELY_APPROVED,
+}
+
+@Entity({ name: 'inovices' })
+@ObjectType()
+export class Invoice {
+  @PrimaryGeneratedColumn()
+  @Field((type) => ID)
+  id: number
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  @Field()
+  created_at: Date
+
+  @JoinColumn({ name: 'user_id' })
+  @Field((type) => User)
+  created_by: User
+
+  @JoinColumn({ name: 'company_id' })
+  @Field((type) => Company)
+  company: Company
+
+  @Column({ type: 'enum', enum: InvoiceStatus })
+  @Field((type) => InvoiceStatus)
+  status: InvoiceStatus
+}
--- back/src/invoices/invoices.module.ts
+++ back/src/invoices/invoices.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common'
 import { InvoicesResolver } from './invoices.resolver'
 import { InvoicesService } from './invoices.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Invoice } from './invoice'
+import { UsersModule } from 'src/users/users.module'
+import { UsersService } from 'src/users/users.service'
+import { CompaniesModule } from 'src/companies/companies.module'
+import { CompaniesService } from 'src/companies/companies.service'
 
 @Module({
-  providers: [InvoicesResolver, InvoicesService],
+  imports: [TypeOrmModule.forFeature([Invoice]), UsersModule, CompaniesModule],
+  providers: [
+    InvoicesResolver,
+    InvoicesService,
+    UsersService,
+    CompaniesService,
+  ],
 })
 export class InvoicesModule {}
