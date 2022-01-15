28db35fcbf799056ae7b0be5a74c899328d7d924
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-01-15 21:03:35 +0900

    add: region

--- back/src/aws/cognito/cognito.service.ts
+++ back/src/aws/cognito/cognito.service.ts
@@ -7,7 +7,9 @@ export class CognitoService {
   private client: CognitoIdentityServiceProvider
   protected user: GetUserResponse
   constructor() {
-    this.client = new CognitoIdentityServiceProvider()
+    this.client = new CognitoIdentityServiceProvider({
+      region: 'ap-northeast-1',
+    })
   }
   public async getUserByToken(token: string): Promise<GetUserResponse> {
     this.user = await this.client
