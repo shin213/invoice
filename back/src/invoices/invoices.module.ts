88c917e49aa82277668008c7637465499548f01a
hamadatakaki <i16hamadat@gmail.com>
2022-01-16 05:40:08 +0900

    upd: reset Invoice module setting

--- back/src/invoices/invoices.module.ts
+++ back/src/invoices/invoices.module.ts
@@ -3,18 +3,9 @@ import { InvoicesResolver } from './invoices.resolver'
 import { InvoicesService } from './invoices.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Invoice } from './invoice'
-import { UsersModule } from 'src/users/users.module'
-import { UsersService } from 'src/users/users.service'
-import { CompaniesModule } from 'src/companies/companies.module'
-import { CompaniesService } from 'src/companies/companies.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Invoice]), UsersModule, CompaniesModule],
-  providers: [
-    InvoicesResolver,
-    InvoicesService,
-    UsersService,
-    CompaniesService,
-  ],
+  imports: [TypeOrmModule.forFeature([Invoice])],
+  providers: [InvoicesResolver, InvoicesService],
 })
 export class InvoicesModule {}
