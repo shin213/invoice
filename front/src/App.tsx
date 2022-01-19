d4a3c7d3e208fa73396abb3f3bb67487a86c3318
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-01-19 13:26:56 +0900

    add: auth routing

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,21 +1,37 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, RouteProps, Routes } from 'react-router-dom'
 import ApprovalsPage from './pages/approvals'
 import HomePage from './pages'
 import NotFoundPage from './pages/logout/ NotFoundPage'
 import RegistrationsPage from './pages/registrations'
 import RequestsPage from './pages/requests'
 import SettingsPage from './pages/settings'
+import { useUser } from './libs/cognito'
+
+const PrivateRoutes: React.VFC = () => {
+  const user = useUser()
+
+  if (user == null) {
+    return <Navigate to="/login" />
+  } else {
+    return (
+      <Routes>
+        <Route path="" element={<HomePage />} />
+        <Route path="registrations" element={<RegistrationsPage />} />
+        <Route path="requests" element={<RequestsPage />} />
+        <Route path="approvals" element={<ApprovalsPage />} />
+        <Route path="settings" element={<SettingsPage />} />
+      </Routes>
+    )
+  }
+}
 
 export default function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/registrations" element={<RegistrationsPage />} />
-        <Route path="/requests" element={<RequestsPage />} />
-        <Route path="/approvals" element={<ApprovalsPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/login" element={<HomePage />} />
+        <Route path="/*" element={<PrivateRoutes />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
