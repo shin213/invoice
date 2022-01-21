4996c7a5bbfd62bbf903d312f35d79cefcb2b66a
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-01-21 22:13:15 +0900

    add: signinのフロント

--- front/src/pages/signin.tsx
+++ front/src/pages/signin.tsx
@@ -3,6 +3,8 @@ import { AuthenticationDetails, CognitoUser } from 'amazon-cognito-identity-js'
 
 import { userPool } from '../lib/cognito'
 import { useNavigate } from 'react-router-dom'
+import { Flex, Box, Heading, Divider, Stack, Input } from '@chakra-ui/react'
+import { PrimaryButton } from '../components/atoms/PrimaryButtom'
 
 export const SignInPage: React.VFC = () => {
   const navigate = useNavigate()
@@ -29,6 +31,7 @@ export const SignInPage: React.VFC = () => {
         Pool: userPool,
       })
 
+      // TODO: ローディングを表示
       cognitoUser.authenticateUser(authenticationDetails, {
         onSuccess: () => {
           // const accessToken = result.getAccessToken().getJwtToken()
@@ -45,21 +48,23 @@ export const SignInPage: React.VFC = () => {
   )
 
   return (
-    <>
-      <h1>Sign In</h1>
-      <form>
-        <fieldset>
-          <legend>User Info</legend>
-          <div>
-            Email: <input type="email" value={email} onChange={onChangeEmail} />
-          </div>
-          <div>
-            Password:
-            <input type="password" value={password} onChange={onChangePassword} />
-          </div>
-        </fieldset>
-        <button onClick={onSignInSubmit}>Sign In</button>
-      </form>
-    </>
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          Invoice
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="メールアドレス" type="email" value={email} onChange={onChangeEmail} />
+          <Input
+            placeholder="パスワード"
+            type="password"
+            value={password}
+            onChange={onChangePassword}
+          />
+          <PrimaryButton onClick={onSignInSubmit}>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
   )
 }
