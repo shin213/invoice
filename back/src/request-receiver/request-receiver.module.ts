6aed169435d5084b8f4f6d7b605c20cb49a60e05
hamadatakaki <i16hamadat@gmail.com>
2022-02-01 06:27:22 +0900

    fix: modify RequestReceiversService.create

--- back/src/request-receiver/request-receiver.module.ts
+++ back/src/request-receiver/request-receiver.module.ts
@@ -1,12 +1,33 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { CompaniesModule } from 'src/companies/companies.module'
+import { CompaniesService } from 'src/companies/companies.service'
+import { InvoicesModule } from 'src/invoices/invoices.module'
+import { InvoicesService } from 'src/invoices/invoices.service'
+import { RequestsModule } from 'src/requests/requests.module'
+import { RequestsService } from 'src/requests/requests.service'
+import { UsersModule } from 'src/users/users.module'
+import { UsersService } from 'src/users/users.service'
 import { RequestReceiver } from './request-receiver'
 import { RequestReceiverResolver } from './request-receiver.resolver'
 import { RequestReceiversService } from './request-receiver.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RequestReceiver])],
-  providers: [RequestReceiverResolver, RequestReceiversService],
+  imports: [
+    TypeOrmModule.forFeature([RequestReceiver]),
+    RequestsModule,
+    UsersModule,
+    InvoicesModule,
+    CompaniesModule,
+  ],
+  providers: [
+    RequestReceiverResolver,
+    RequestReceiversService,
+    RequestsService,
+    UsersService,
+    InvoicesService,
+    CompaniesService,
+  ],
   exports: [RequestReceiverModule, TypeOrmModule],
 })
 export class RequestReceiverModule {}
