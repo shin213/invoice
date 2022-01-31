68cc8c716a68f6cf3024d79444a4d0390c8fb741
hamadatakaki <i16hamadat@gmail.com>
2022-02-01 06:25:48 +0900

    fix: modify RequestsService.create

--- back/src/requests/requests.module.ts
+++ back/src/requests/requests.module.ts
@@ -3,10 +3,27 @@ import { RequestsService } from './requests.service'
 import { RequestsResolver } from './requests.resolver'
 import { Request } from './request'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { UsersModule } from 'src/users/users.module'
+import { InvoicesModule } from 'src/invoices/invoices.module'
+import { CompaniesModule } from 'src/companies/companies.module'
+import { UsersService } from 'src/users/users.service'
+import { InvoicesService } from 'src/invoices/invoices.service'
+import { CompaniesService } from 'src/companies/companies.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Request])],
-  providers: [RequestsService, RequestsResolver],
+  imports: [
+    TypeOrmModule.forFeature([Request]),
+    UsersModule,
+    InvoicesModule,
+    CompaniesModule,
+  ],
+  providers: [
+    RequestsService,
+    RequestsResolver,
+    UsersService,
+    InvoicesService,
+    CompaniesService,
+  ],
   exports: [RequestsModule, TypeOrmModule],
 })
 export class RequestsModule {}
