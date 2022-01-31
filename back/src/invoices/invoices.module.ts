1b8227fdc4e98c64803fdce2273a16302aa139e5
hamadatakaki <i16hamadat@gmail.com>
2022-02-01 01:06:20 +0900

    fix: modify InvoicesService.create

--- back/src/invoices/invoices.module.ts
+++ back/src/invoices/invoices.module.ts
@@ -3,10 +3,19 @@ import { InvoicesResolver } from './invoices.resolver'
 import { InvoicesService } from './invoices.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Invoice } from './invoice'
+import { UsersModule } from 'src/users/users.module'
+import { UsersService } from 'src/users/users.service'
+import { CompaniesModule } from 'src/companies/companies.module'
+import { CompaniesService } from 'src/companies/companies.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Invoice])],
-  providers: [InvoicesResolver, InvoicesService],
+  imports: [TypeOrmModule.forFeature([Invoice]), UsersModule, CompaniesModule],
+  providers: [
+    InvoicesResolver,
+    InvoicesService,
+    UsersService,
+    CompaniesService,
+  ],
   exports: [InvoicesModule, TypeOrmModule],
 })
 export class InvoicesModule {}
--- back/src/invoices/invoices.service.ts
+++ back/src/invoices/invoices.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { CompaniesService } from 'src/companies/companies.service'
 import { Company } from 'src/companies/company'
 import { User } from 'src/users/user'
+import { UsersService } from 'src/users/users.service'
 import { Repository } from 'typeorm'
 import { NewInvoiceInput } from './dto/newInvoice.input'
 import { Invoice } from './invoice'
@@ -11,6 +13,8 @@ export class InvoicesService {
   constructor(
     @InjectRepository(Invoice)
     private invoicesRepository: Repository<Invoice>,
+    private usersService: UsersService,
+    private companiesService: CompaniesService,
   ) {}
 
   findAll(): Promise<Invoice[]> {
@@ -39,6 +43,8 @@ export class InvoicesService {
 
   async create(data: NewInvoiceInput): Promise<Invoice> {
     const invoice = this.invoicesRepository.create(data)
+    invoice.created_by = await this.usersService.findOneById(data.user_id)
+    invoice.company = await this.companiesService.findOneById(data.company_id)
     await this.invoicesRepository.save(invoice)
     return invoice
   }
