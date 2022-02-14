a676ebf9c0305b0de3a5d3c80b136d54916bcc77
Kasega0 <shimomura@hapis.k.u-tokyo.ac.jp>
2022-02-14 18:14:45 +0900

    add: new invoice list page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -11,6 +11,7 @@ import InvoiceDetailPage from './pages/invoices/[id]'
 import RequestSendPage from './pages/invoices/[id]/request'
 import InquirySendPage from './pages/invoices/[id]/inquiry'
 import ApprovalSendPage from './pages/invoices/[id]/approval'
+import IssueListPage from './pages/issue/list'
 import StorePage from './pages/store'
 
 const PrivateRoutes: React.VFC = () => {
@@ -28,6 +29,7 @@ const PrivateRoutes: React.VFC = () => {
         <Route path="invoices/:id/request" element={<RequestSendPage />} />
         <Route path="invoices/:id/inquiry" element={<InquirySendPage />} />
         <Route path="invoices/:id/approval" element={<ApprovalSendPage />} />
+        <Route path="issue" element={<IssueListPage />} />
         <Route path="store" element={<StorePage />} />
         <Route path="settings" element={<SettingsPage />} />
         <Route path="*" element={<NotFoundPage />} />
