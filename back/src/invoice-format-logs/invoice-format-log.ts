2bc5ac3cebd3f093adfbdd6919a2d152cd509624
Kasega0 <shimomura@hapis.k.u-tokyo.ac.jp>
2022-03-04 21:19:26 +0900

    feat: add common elements to InvoiceFormatLog

--- back/src/invoice-format-logs/invoice-format-log.ts
+++ back/src/invoice-format-logs/invoice-format-log.ts
@@ -27,6 +27,39 @@ export class InvoiceFormatLog {
   @Field({ nullable: false })
   readonly createdAt!: Date
 
+  // 全請求書の共通項目（該当のInvoiceFormatElement.idを持つ）
+  @Column({
+    nullable: true,
+    type: 'varchar',
+    comment: '[共通項目] 工事名',
+  })
+  @Field({ nullable: true })
+  constructionNameId: string | null = null
+
+  @Column({
+    nullable: true,
+    type: 'varchar',
+    comment: '[共通項目] 請求日',
+  })
+  @Field({ nullable: true })
+  billingDateId: string | null = null
+
+  @Column({
+    nullable: true,
+    type: 'varchar',
+    comment: '[共通項目] 支払期限',
+  })
+  @Field({ nullable: true })
+  paymentDeadlineId: string | null = null
+
+  @Column({
+    nullable: true,
+    type: 'varchar',
+    comment: '[共通項目] 支払金額',
+  })
+  @Field({ nullable: true })
+  paymentAmountId: string | null = null
+
   @ManyToOne((type) => InvoiceFormat, (format) => format.invoiceFormatsLogs, {
     nullable: false,
   })
