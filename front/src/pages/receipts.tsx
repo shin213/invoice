a1f4fcbc40a8e7423cfc96d8e94ca3521bf306a9
hamadatakaki <i16hamadat@gmail.com>
2022-03-06 12:18:43 +0900

    add: receipt route and receipts list page

---
+++ front/src/pages/receipts.tsx
@@ -0,0 +1,21 @@
+import { Box, Heading, Stack } from '@chakra-ui/react'
+import React from 'react'
+import InvoicesTable from '../components/molecules/InvoicesTable'
+// import UsersTable from '../components/molecules/UsersTable'
+import LoginTemplate from '../components/templates/LoginTemplate'
+
+
+const ReceiptsPage: React.VFC = () => (
+  <LoginTemplate>
+    <Stack>
+      <Heading as="h1" size="md" textAlign="center">
+        受領
+      </Heading>
+      <Box bg="white" p={4} borderRadius="md" shadow="md">
+        <InvoicesTable />
+      </Box>
+    </Stack>
+  </LoginTemplate>
+)
+
+export default ReceiptsPage
