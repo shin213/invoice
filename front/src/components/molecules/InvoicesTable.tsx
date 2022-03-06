37575fa2d367bb5e35691b7a21f5560e8174108c
hamadatakaki <i16hamadat@gmail.com>
2022-03-07 02:59:39 +0900

    refactoring

--- front/src/components/molecules/InvoicesTable.tsx
+++ front/src/components/molecules/InvoicesTable.tsx
@@ -26,52 +26,60 @@ export type InvoicesTableProps = {
   invoices: InvoicesTableLineProp[]
 }
 
-const InvoicesTable: React.VFC<InvoicesTableProps> = ({ invoices }: InvoicesTableProps) => {
+const InvoicesTableLine = (invoice: InvoicesTableLineProp) => {
+  console.log(invoice)
+
   const navigate = useNavigate()
+  const detailURL = `/invoices/${invoice.id}`
+  const constructionName = invoice.construction ? invoice.construction.name : '-'
+
   return (
-    <Table variant="striped">
-      <Thead>
-        <Tr>
-          <Th>企業名</Th>
-          <Th>工事名</Th>
-          <Th>請求日</Th>
-          <Th>支払い期限</Th>
-          <Th>支払い金額</Th>
-          <Th>ステータス</Th>
-          <Th>原本表示</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {invoices.map((invoice) => (
-          <Tr key={invoice.id}>
-            <Td>{invoice.company.name}</Td>
-            <Td>{invoice.construction ? invoice.construction.name : '-'}</Td>
-            <Td>{invoice.billingDate}</Td>
-            <Td>{invoice.dueDateForPayment}</Td>
-            <Td isNumeric>{invoice.paymentAmount}</Td>
-            <Td>{invoice.status}</Td>
-            <Td>
-              <Button bgColor="cyan.500" color="white" onClick={() => navigate(`/invoices/${invoice.id}`)}>
-                <MdOpenInNew title="確認" />
-                <Box p="2">確認</Box>
-              </Button>
-            </Td>
-          </Tr>
-        ))}
-      </Tbody>
-      <Tfoot>
-        <Tr>
-          <Th>企業名</Th>
-          <Th>工事名</Th>
-          <Th>請求日</Th>
-          <Th>支払い期限</Th>
-          <Th>支払い金額</Th>
-          <Th>ステータス</Th>
-          <Th>原本表示</Th>
-        </Tr>
-      </Tfoot>
-    </Table>
+    <Tr key={invoice.id}>
+      <Td>{invoice.company.name}</Td>
+      <Td>{constructionName}</Td>
+      <Td>{invoice.billingDate}</Td>
+      <Td>{invoice.dueDateForPayment}</Td>
+      <Td isNumeric>{invoice.paymentAmount}</Td>
+      <Td>{invoice.status}</Td>
+      <Td>
+        <Button bgColor="cyan.500" color="white" onClick={() => navigate(detailURL)}>
+          <MdOpenInNew title="確認" />
+          <Box p="2">確認</Box>
+        </Button>
+      </Td>
+    </Tr>
   )
 }
+
+const InvoicesTable: React.VFC<InvoicesTableProps> = ({ invoices }: InvoicesTableProps) => (
+  <Table variant="striped">
+    <Thead>
+      <Tr>
+        <Th>企業名</Th>
+        <Th>工事名</Th>
+        <Th>請求日</Th>
+        <Th>支払い期限</Th>
+        <Th>支払い金額</Th>
+        <Th>ステータス</Th>
+        <Th>原本表示</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      {invoices.map((invoice) => InvoicesTableLine(invoice))}
+    </Tbody>
+    <Tfoot>
+      <Tr>
+        <Th>企業名</Th>
+        <Th>工事名</Th>
+        <Th>請求日</Th>
+        <Th>支払い期限</Th>
+        <Th>支払い金額</Th>
+        <Th>ステータス</Th>
+        <Th>原本表示</Th>
+      </Tr>
+    </Tfoot>
+  </Table>
+)
+
 
 export default InvoicesTable
