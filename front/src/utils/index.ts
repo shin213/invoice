8fa0035fced20b6e51f00d8255ccbdb084a319ac
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-04-05 20:10:36 +0900

    refac: mutationOptions

--- front/src/utils/index.ts
+++ front/src/utils/index.ts
@@ -42,8 +42,9 @@ export function mutationOptionsWithMsg<T, U>(
   message: string,
   errorMessageTranslation: Record<string, string> = { '': '不明なエラーです。' },
 ): MutationHookOptions<T, U, DefaultContext, ApolloCache<unknown>> {
-  return {
-    onCompleted() {
+  return mutationOptions(
+    toast,
+    () => {
       toast({
         description: message,
         status: 'success',
@@ -51,20 +52,6 @@ export function mutationOptionsWithMsg<T, U>(
         isClosable: true,
       })
     },
-    onError(err) {
-      const messages = err.graphQLErrors.map((e) => e.message)
-      if (messages.length > 1) {
-        console.error(messages)
-      } else if (messages.length === 0) {
-        console.error('messages.length === 0')
-        messages.push('不明なエラーが発生しました。')
-      }
-      toast({
-        description: errorMessageTranslation[messages[0] ?? ''] || '不明なエラーです。',
-        status: 'error',
-        position: 'top',
-        isClosable: true,
-      })
-    },
-  }
+    errorMessageTranslation,
+  )
 }
