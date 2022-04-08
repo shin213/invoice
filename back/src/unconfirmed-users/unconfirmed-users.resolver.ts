9bd678df8dcf4c4497892ea83b89fcda5cb263c7
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-04-08 11:37:47 +0900

    feat:add and update unconfirmed users by users

--- back/src/unconfirmed-users/unconfirmed-users.resolver.ts
+++ back/src/unconfirmed-users/unconfirmed-users.resolver.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NotFoundException, UseGuards } from '@nestjs/common'
+import { UseGuards } from '@nestjs/common'
 import {
   Args,
   Mutation,
@@ -9,25 +9,29 @@ import {
   Resolver,
 } from '@nestjs/graphql'
 import { AdminAuthorizerGuard } from 'src/aws/admin-authorizer/admin-authorizer.guard'
+import { CurrentUser } from 'src/aws/authorizer/authorizer.decorator'
 import { AuthorizerGuard } from 'src/aws/authorizer/authorizer.guard'
+import { AuthUser } from 'src/aws/cognito/cognito'
 import { Company } from 'src/companies/company'
 import { AdminNewUnconfirmedUserInput } from './dto/adminNewUnconfirmedUser.input'
+import { NewUnconfirmedUserInput } from './dto/newUnconfirmedUser.input'
+import { UpdateUnconfirmedUserInput } from './dto/updateUnconfirmedUser.input'
 import { UnconfirmedUser } from './unconfirmed-user'
 import { UnconfirmedUsersService } from './unconfirmed-users.service'
 
 @Resolver((of: unknown) => UnconfirmedUser)
 export class UnconfirmedUsersResolver {
-  constructor(private unconfirmedUsersService: UnconfirmedUsersService) {}
+  constructor(private service: UnconfirmedUsersService) {}
 
   @UseGuards(AdminAuthorizerGuard)
   @Query((returns) => [UnconfirmedUser])
   adminUnconfirmedUsers(): Promise<UnconfirmedUser[]> {
-    return this.unconfirmedUsersService.findAll()
+    return this.service.findAll()
   }
 
   @ResolveField('company')
   async company(@Parent() user: UnconfirmedUser): Promise<Company> {
-    return this.unconfirmedUsersService.company(user.email)
+    return this.service.company(user.email)
   }
 
   @UseGuards(AdminAuthorizerGuard)
@@ -36,7 +40,7 @@ export class UnconfirmedUsersResolver {
     @Args('newUnconfirmedUser') newUser: AdminNewUnconfirmedUserInput,
   ): Promise<UnconfirmedUser> {
     const user = { ...newUser }
-    return this.unconfirmedUsersService.create(user)
+    return this.service.create(user)
   }
 
   @UseGuards(AdminAuthorizerGuard)
@@ -44,6 +48,34 @@ export class UnconfirmedUsersResolver {
   async adminRemoveUnconfirmedUser(
     @Args({ name: 'email', type: () => String }) email: string,
   ) {
-    return this.unconfirmedUsersService.remove(email)
+    return this.service.remove(email)
+  }
+
+  @UseGuards(AuthorizerGuard)
+  @Mutation((returns) => UnconfirmedUser)
+  addUnconfirmedUser(
+    @CurrentUser() currentUser: AuthUser,
+    @Args('newUnconfirmedUser') newUser: NewUnconfirmedUserInput,
+  ): Promise<UnconfirmedUser> {
+    const user = { ...newUser, companyId: currentUser.dbUser.companyId }
+    return this.service.create(user)
+  }
+
+  @UseGuards(AuthorizerGuard)
+  @Mutation((returns) => UnconfirmedUser)
+  updateUnconfirmedUser(
+    @CurrentUser() currentUser: AuthUser,
+    @Args('updateUnconfirmedUser') updateUser: UpdateUnconfirmedUserInput,
+  ): Promise<UnconfirmedUser> {
+    return this.service.update(currentUser.dbUser, updateUser)
+  }
+
+  @UseGuards(AdminAuthorizerGuard)
+  @Mutation((returns) => UnconfirmedUser)
+  adminUpdateUnconfirmedUser(
+    @CurrentUser() currentUser: AuthUser,
+    @Args('updateUnconfirmedUser') updateUser: UpdateUnconfirmedUserInput,
+  ): Promise<UnconfirmedUser> {
+    return this.service.updateWithoutCheckingCompany(updateUser)
   }
 }
--- back/src/unconfirmed-users/unconfirmed-users.service.ts
+++ back/src/unconfirmed-users/unconfirmed-users.service.ts
@@ -1,8 +1,10 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Company } from 'src/companies/company'
+import { User } from 'src/users/user'
 import { Repository } from 'typeorm'
 import { AdminNewUnconfirmedUserInput } from './dto/adminNewUnconfirmedUser.input'
+import { UpdateUnconfirmedUserInput } from './dto/updateUnconfirmedUser.input'
 import { UnconfirmedUser } from './unconfirmed-user'
 
 @Injectable()
@@ -35,6 +37,40 @@ export class UnconfirmedUsersService {
     const unconfirmedUser = this.unconfirmedUsersRepository.create(data)
     await this.unconfirmedUsersRepository.save(unconfirmedUser)
     return unconfirmedUser
+  }
+
+  async updateWithoutCheckingCompany(data: UpdateUnconfirmedUserInput) {
+    const unconfirmedUser = await this.findOneByEmail(data.email)
+    if (unconfirmedUser == undefined) {
+      throw new HttpException('UnconfirmedUser Not Found', HttpStatus.NOT_FOUND)
+    }
+    return await this._update(unconfirmedUser, data)
+  }
+
+  async update(currentUser: User, data: UpdateUnconfirmedUserInput) {
+    const unconfirmedUser = await this.findOneByEmail(data.email)
+    if (currentUser.isAdmin === false) {
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN)
+    }
+    if (
+      unconfirmedUser == undefined ||
+      unconfirmedUser.companyId !== currentUser.companyId
+    ) {
+      throw new HttpException('UnconfirmedUser Not Found', HttpStatus.NOT_FOUND)
+    }
+    return await this._update(unconfirmedUser, data)
+  }
+
+  private async _update(
+    baseData: UnconfirmedUser,
+    data: UpdateUnconfirmedUserInput,
+  ): Promise<UnconfirmedUser> {
+    const updatedUnconfirmedUser = this.unconfirmedUsersRepository.merge(
+      baseData,
+      data,
+    )
+    await this.unconfirmedUsersRepository.save(updatedUnconfirmedUser)
+    return updatedUnconfirmedUser
   }
 
   async remove(id: string): Promise<boolean> {
