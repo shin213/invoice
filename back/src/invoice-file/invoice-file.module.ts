04c10ad1937d9f3f91495e439ec7c336bd04dc97
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-04-11 20:23:57 +0900

    feat: useGuard for file upload

--- back/src/invoice-file/invoice-file.module.ts
+++ back/src/invoice-file/invoice-file.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { InvoiceFileService } from './invoice-file.service'
 import { InvoiceFileResolver } from './invoice-file.resolver'
+import { CognitoModule } from 'src/aws/cognito/cognito.module'
 
 @Module({
+  imports: [CognitoModule],
   providers: [InvoiceFileService, InvoiceFileResolver],
 })
 export class InvoiceFileModule {}
--- back/src/invoice-file/invoice-file.resolver.ts
+++ back/src/invoice-file/invoice-file.resolver.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { UseGuards } from '@nestjs/common'
 import { Args, Mutation, Resolver } from '@nestjs/graphql'
 import { FileUpload, GraphQLUpload } from 'graphql-upload'
+import { AuthorizerGuard } from 'src/aws/authorizer/authorizer.guard'
 
 @Resolver()
 export class InvoiceFileResolver {
+  @UseGuards(AuthorizerGuard)
   @Mutation((returns) => Boolean)
   async uploadInvoiceFile(
     @Args({ name: 'file', type: () => GraphQLUpload }) file: FileUpload,
