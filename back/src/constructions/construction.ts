9491716cb4502140f55d06579766995e00566ce1
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-04-17 22:43:15 +0900

    feat: add shownName in constructions

--- back/src/constructions/construction.ts
+++ back/src/constructions/construction.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Field, Int, ObjectType } from '@nestjs/graphql'
+import { Field, Int, ObjectType, registerEnumType } from '@nestjs/graphql'
 import { Company } from 'src/companies/company'
 import { ConstructionUser } from 'src/construction-user/construction-user'
 import { Invoice } from 'src/invoices/invoice'
@@ -12,7 +12,16 @@ import {
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm'
+
+export enum ShownName {
+  name = 'name',
+  code = 'code',
+  custom = 'custom',
+}
+
+registerEnumType(ShownName, { name: 'ShownName' })
 
 @Entity({ name: 'constructions' })
 @ObjectType()
@@ -21,17 +30,46 @@ export class Construction {
   @Field((type) => Int)
   readonly id!: number
 
+  @Column('varchar', { nullable: false })
+  @Field((type) => String)
+  name!: string
+
   @Column('varchar', { length: '256', nullable: false, default: '' })
   @Field((type) => String, { nullable: false })
   code!: string
+
+  @Column({
+    type: 'enum',
+    enum: ShownName,
+    comment:
+      '協力企業に工事名を表示するか工事コード、カスタム表示名を表示するか',
+    default: ShownName.name,
+  })
+  @Field((type) => ShownName, {
+    description:
+      '協力企業に工事名を表示するか工事コード、カスタム表示名を表示するか',
+  })
+  shownName!: ShownName
+
+  @Column('varchar', {
+    length: '256',
+    default: '',
+    comment: 'カスタム表示名',
+  })
+  @Field((type) => String, { description: 'カスタム表示名' })
+  customShownName!: string
+
+  @Column('text', { default: '', comment: '備考' })
+  @Field((type) => String, { description: '備考' })
+  remarks!: string
 
   @CreateDateColumn({ type: 'timestamptz', nullable: false })
   @Field({ nullable: false })
   createdAt!: Date
 
-  @Column('varchar', { nullable: false })
-  @Field((type) => String)
-  name!: string
+  @UpdateDateColumn({ type: 'timestamptz', nullable: false })
+  @Field({ nullable: false })
+  updatedAt!: Date
 
   @Column({ nullable: false })
   @Field((type) => Int)
--- back/src/constructions/dto/newConstruction.input.ts
+++ back/src/constructions/dto/newConstruction.input.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Field, InputType } from '@nestjs/graphql'
 import { MaxLength } from 'class-validator'
+import { ShownName } from '../construction'
 
 @InputType()
 export class NewConstructionInput {
@@ -11,6 +12,15 @@ export class NewConstructionInput {
   @Field()
   @MaxLength(256)
   code!: string
+
+  @Field((type) => ShownName)
+  shownName!: ShownName
+
+  @Field()
+  customShownName!: string
+
+  @Field()
+  remarks!: string
 
   @Field((type) => [String])
   userIds!: string[]
