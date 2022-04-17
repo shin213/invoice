abbc7aec05c91be42757e2ebbe09c262f53d315b
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-04-17 23:25:56 +0900

    feat: add constraints on construction name

--- back/src/constructions/dto/newConstruction.input.ts
+++ back/src/constructions/dto/newConstruction.input.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Field, InputType } from '@nestjs/graphql'
-import { MaxLength } from 'class-validator'
+import { Length, MaxLength } from 'class-validator'
 import { ShownName } from '../construction'
 
 @InputType()
 export class NewConstructionInput {
   @Field()
-  @MaxLength(256)
+  @Length(1, 256)
   name!: string
 
   @Field()
