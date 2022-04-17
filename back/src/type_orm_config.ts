2179ece757b8ff8d09a5fb45ee8933d730151ee2
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-04-17 18:55:47 +0900

    refac: create typeorm entities

--- back/src/type_orm_config.ts
+++ back/src/type_orm_config.ts
@@ -1,21 +1,6 @@
-import { Company } from './companies/company'
-import { User } from './users/user'
-import { InvoiceFormat } from './invoice-formats/invoice-format'
-import { InvoiceFormatLog } from './invoice-format-logs/invoice-format-log'
-import { InvoiceFormatDetailElement } from './invoice-format-detail-elements/invoice-format-detail-element'
-import { InvoiceFormatElement } from './invoice-format-elements/invoice-format-element'
-import { Comment } from './comments/comment'
-import { Invoice } from './invoices/invoice'
-import { Request } from './requests/request'
-import { RequestReceiver } from './request-receiver/request-receiver'
-import { RequestNotification } from './request-notifications/request-notification'
-import { Judgement } from './judgements/judgement'
-import { PartnerCompany } from './partner-companies/partner-company'
-import { Construction } from './constructions/construction'
 import TypeOrmNamingStrategy from './type_orm_naming_strategy'
-import { UnconfirmedUser } from './unconfirmed-users/unconfirmed-user'
 import 'dotenv/config'
-import { InvoiceFile } from './invoice-files/invoice-file'
+import { typeOrmEntities } from './type_orm_entities'
 
 export default {
   type: 'postgres' as const,
@@ -24,24 +9,7 @@ export default {
   username: process.env.POSTGRES_USER || 'admin',
   password: process.env.POSTGRES_PASSWORD || 'dev_sample',
   database: process.env.POSTGRES_DB || 'main_db',
-  entities: [
-    Company,
-    PartnerCompany,
-    Construction,
-    User,
-    InvoiceFormat,
-    InvoiceFormatLog,
-    InvoiceFormatDetailElement,
-    InvoiceFormatElement,
-    Comment,
-    Invoice,
-    InvoiceFile,
-    Request,
-    RequestReceiver,
-    RequestNotification,
-    Judgement,
-    UnconfirmedUser,
-  ],
+  entities: typeOrmEntities,
   migrations: ['./src/migrations/*.ts'],
   cli: {
     migrationsDir: './src/migrations',
---
+++ back/src/type_orm_entities.ts
@@ -0,0 +1,33 @@
+import { Company } from './companies/company'
+import { Construction } from './constructions/construction'
+import { InvoiceFile } from './invoice-files/invoice-file'
+import { InvoiceFormatDetailElement } from './invoice-format-detail-elements/invoice-format-detail-element'
+import { InvoiceFormatElement } from './invoice-format-elements/invoice-format-element'
+import { InvoiceFormatLog } from './invoice-format-logs/invoice-format-log'
+import { InvoiceFormat } from './invoice-formats/invoice-format'
+import { Invoice } from './invoices/invoice'
+import { Judgement } from './judgements/judgement'
+import { PartnerCompany } from './partner-companies/partner-company'
+import { RequestNotification } from './request-notifications/request-notification'
+import { RequestReceiver } from './request-receiver/request-receiver'
+import { UnconfirmedUser } from './unconfirmed-users/unconfirmed-user'
+import { User } from './users/user'
+
+export const typeOrmEntities = [
+  Company,
+  PartnerCompany,
+  Construction,
+  User,
+  InvoiceFormat,
+  InvoiceFormatLog,
+  InvoiceFormatDetailElement,
+  InvoiceFormatElement,
+  Comment,
+  Invoice,
+  InvoiceFile,
+  Request,
+  RequestReceiver,
+  RequestNotification,
+  Judgement,
+  UnconfirmedUser,
+]
