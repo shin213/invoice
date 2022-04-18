f5adb0f43cfffbc747b35277954208048cb0b8f5
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-04-18 10:13:41 +0900

    feat: add construction-user

--- back/src/construction-user/construction-user.module.ts
+++ back/src/construction-user/construction-user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ConstructionUserService } from './construction-user.service'
 import { ConstructionUserResolver } from './construction-user.resolver'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConstructionUser } from './construction-user'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ConstructionUser])],
   providers: [ConstructionUserService, ConstructionUserResolver],
+  exports: [ConstructionUserModule, ConstructionUserService, TypeOrmModule],
 })
 export class ConstructionUserModule {}
--- back/src/constructions/constructions.service.ts
+++ back/src/constructions/constructions.service.ts
@@ -1,6 +1,8 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { ConstructionUserService } from 'src/construction-user/construction-user.service'
 import { User } from 'src/users/user'
+import { UsersService } from 'src/users/users.service'
 import { Repository } from 'typeorm'
 import { Construction } from './construction'
 import { NewConstructionInput } from './dto/newConstruction.input'
@@ -10,6 +12,8 @@ export class ConstructionsService {
   constructor(
     @InjectRepository(Construction)
     private constructionsRepository: Repository<Construction>,
+    private constructionUserService: ConstructionUserService,
+    private usersService: UsersService,
   ) {}
 
   findAll(): Promise<Construction[]> {
@@ -36,11 +40,11 @@ export class ConstructionsService {
     })
     await this.constructionsRepository.save(construction)
     for (const userId of data.userIds) {
-      await this.constructionsRepository
-        .createQueryBuilder()
-        .relation(Construction, 'constructionUsers')
-        .of(construction)
-        .add(userId)
+      const user = await this.usersService.findOneById(userId)
+      if (user == undefined || user.companyId !== companyId) {
+        throw new HttpException('User Not Found', HttpStatus.NOT_FOUND)
+      }
+      await this.constructionUserService.create(construction.id, userId)
     }
     return construction
   }
